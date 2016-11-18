86e014e2f250ef5aba72f7ad457741859d969e7f
Dhian Pratama <dhian.p@archipelagoasia.com>
2016-11-18 14:19:25 +0700

    Update all headers

--- resources/views/yct_public/about-us.blade.php
+++ resources/views/yct_public/about-us.blade.php
@@ -37,11 +37,13 @@
     <body id="home" class="wide body-light multipage multipage-sub">
 
         <!-- Preloader -->
+        <!--
         <div id="preloader">
             <div id="status">
                 <div class="spinner"></div>
             </div>
         </div>
+        -->
 
         <!-- Wrap all content -->
         <div class="wrapper">
@@ -286,72 +288,13 @@
 
             <!-- FOOTER -->
             <footer class="footer">
-                <div class="footer-widgets">
-                    <div class="container">
-                        <div class="row">
-
-                            <div class="col-md-3">
-                                <div class="widget widget-about">
-                                    <h4 class="widget-title">About Us</h4>
-                                    <p>Fusce pellentesque velvitae tincidunt egestas. Pellentesque habitant morbi.Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis.</p>
-                                    <address>
-                                        <div><i class="fa fa-phone"></i>+90 555 55 55</div>
-                                        <div><i class="fa fa-envelope"></i> <a href="mailto:info@example.com">info@example.com</a></div>
-                                    </address>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="widget widget-categories">
-                                    <h4 class="widget-title">Popular searches</h4>
-                                    <ul>
-                                        <li><a href="#">Online Registration</a></li>
-                                        <li><a href="#">Sell Event Tickets</a></li>
-                                        <li><a href="#">Post Events</a></li>
-                                        <li><a href="#">Event Planning Software</a></li>
-                                        <li><a href="#">Online Event Management</a></li>
-                                        <li><a href="#">Event Management Software</a></li>
-                                        <li><a href="#">Event Payment</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="widget widget-twitter">
-                                    <h4 class="widget-title">Recent Tweets</h4>
-                                    <ul>
-                                        <li>
-                                            <a href="#">@isamercan</a> Cupcake chocolate cake sweet roll. Gummies macaroon biscuit cupcake candy dragée. <a href="#">#Conference about 2 hours ago</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">@isamercan</a> Cupcake chocolate cake sweet roll. Gummies macaroon biscuit cupcake candy dragée. <a href="#">#Conference about 2 hours ago</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="widget widget-flickr-feed">
-                                    <h4 class="widget-title"><span>Instagram Photos</span></h4>
-                                    <ul>
-                                        <li><a href="#"><img src="assets/img/preview/sidebar-1.jpg" alt=""></a></li>
-                                        <li><a href="#"><img src="assets/img/preview/sidebar-2.jpg" alt=""></a></li>
-                                        <li><a href="#"><img src="assets/img/preview/sidebar-3.jpg" alt=""></a></li>
-                                        <li><a href="#"><img src="assets/img/preview/sidebar-4.jpg" alt=""></a></li>
-                                        <li><a href="#"><img src="assets/img/preview/sidebar-5.jpg" alt=""></a></li>
-                                        <li><a href="#"><img src="assets/img/preview/sidebar-6.jpg" alt=""></a></li>
-                                        <!--li><a href="#"><img src="assets/img/preview/sidebar-7.jpg" alt=""></a></li>
-                                        <li><a href="#"><img src="assets/img/preview/sidebar-8.jpg" alt=""></a></li-->
-                                    </ul>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
                 <div class="footer-meta footer-meta-alt">
                     <div class="container">
 
                         <div class="row">
                             <div class="col-md-9 col-sm-6">
                                 <ul class="footer-menu">
+                                <!--
                                     <li><a href="#">About</a></li>
                                     <li><a href="#">Blog</a></li>
                                     <li><a href="#">Help</a></li>
@@ -361,7 +304,8 @@
                                     <li><a href="#">Terms</a></li>
                                     <li><a href="#">Privacy</a></li>
                                     <li><a href="#">Cookies</a></li>
-                                    <li>All Rights Reserved</li>
+                                    -->
+                                    <li>Copyright © 2016 <span style="font-weight: bold;">Young Creative Thinker.</span>&nbsp; All right reserved</li>
                                 </ul>
                             </div>
                             <div class="col-md-3 col-sm-6">
--- resources/views/yct_public/index_public.blade.php
+++ resources/views/yct_public/index_public.blade.php
@@ -673,65 +673,6 @@
 
             <!-- FOOTER -->
             <footer class="footer">
-                {{--<div class="footer-widgets">--}}
-                    {{--<div class="container">--}}
-                        {{--<div class="row">--}}
-                            {{--<div class="col-md-3">--}}
-                                {{--<div class="widget widget-about">--}}
-                                    {{--<h4 class="widget-title">About Us</h4>--}}
-                                    {{--<p>Fusce pellentesque velvitae tincidunt egestas. Pellentesque habitant morbi.Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis.</p>--}}
-                                    {{--<address>--}}
-                                        {{--<div><i class="fa fa-phone"></i>+90 555 55 55</div>--}}
-                                        {{--<div><i class="fa fa-envelope"></i> <a href="mailto:info@example.com">info@example.com</a></div>--}}
-                                    {{--</address>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                            {{--<div class="col-md-3">--}}
-                                {{--<div class="widget widget-categories">--}}
-                                    {{--<h4 class="widget-title">Popular searches</h4>--}}
-                                    {{--<ul>--}}
-                                        {{--<li><a href="#">Online Registration</a></li>--}}
-                                        {{--<li><a href="#">Sell Event Tickets</a></li>--}}
-                                        {{--<li><a href="#">Post Events</a></li>--}}
-                                        {{--<li><a href="#">Event Planning Software</a></li>--}}
-                                        {{--<li><a href="#">Online Event Management</a></li>--}}
-                                        {{--<li><a href="#">Event Management Software</a></li>--}}
-                                        {{--<li><a href="#">Event Payment</a></li>--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                            {{--<div class="col-md-3">--}}
-                                {{--<div class="widget widget-twitter">--}}
-                                    {{--<h4 class="widget-title">Recent Tweets</h4>--}}
-                                    {{--<ul>--}}
-                                        {{--<li>--}}
-                                            {{--<a href="#">@isamercan</a> Cupcake chocolate cake sweet roll. Gummies macaroon biscuit cupcake candy dragée. <a href="#">#Conference about 2 hours ago</a>--}}
-                                        {{--</li>--}}
-                                        {{--<li>--}}
-                                            {{--<a href="#">@isamercan</a> Cupcake chocolate cake sweet roll. Gummies macaroon biscuit cupcake candy dragée. <a href="#">#Conference about 2 hours ago</a>--}}
-                                        {{--</li>--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                            {{--<div class="col-md-3">--}}
-                                {{--<div class="widget widget-flickr-feed">--}}
-                                    {{--<h4 class="widget-title"><span>Instagram Photos</span></h4>--}}
-                                    {{--<ul>--}}
-                                        {{--<li><a href="#"><img src="assets/img/preview/sidebar-1.jpg" alt=""></a></li>--}}
-                                        {{--<li><a href="#"><img src="assets/img/preview/sidebar-2.jpg" alt=""></a></li>--}}
-                                        {{--<li><a href="#"><img src="assets/img/preview/sidebar-3.jpg" alt=""></a></li>--}}
-                                        {{--<li><a href="#"><img src="assets/img/preview/sidebar-4.jpg" alt=""></a></li>--}}
-                                        {{--<li><a href="#"><img src="assets/img/preview/sidebar-5.jpg" alt=""></a></li>--}}
-                                        {{--<li><a href="#"><img src="assets/img/preview/sidebar-6.jpg" alt=""></a></li>--}}
-                                        {{--<!--li><a href="#"><img src="assets/img/preview/sidebar-7.jpg" alt=""></a></li>--}}
-                                        {{--<li><a href="#"><img src="assets/img/preview/sidebar-8.jpg" alt=""></a></li-->--}}
-                                    {{--</ul>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
                 <div class="footer-meta footer-meta-alt">
                     <div class="container">
 
